596f659e5f10e4a3afc6c98b09c65fedd8c69072
nmoorthy1 <nishantmoorthy524@gmail.com>
2016-07-11 13:49:40 -0700

    Add files via upload

---
+++ dl.py
@@ -0,0 +1,82 @@
+import urllib.request
+import os
+import sys
+from bs4 import BeautifulSoup
+import requests
+import re
+import time
+import zipfile
+
+dir = "./Images " + time.strftime("%m-%d-%Y") + "/"
+a = 0
+b = 0
+c = 0
+try:
+	if len(sys.argv) == 2:
+		c = 1
+		try:
+			print("Trying to open ", sys.argv[1])
+			f = open(sys.argv[1], "r")
+		except OSError as e:
+			print("Could not find ", sys.argv[1])
+			c = 2
+	else:
+		print("No args")
+	if len(sys.argv) == 1 or c == 2:
+		print("Trying to open a.txt\n")
+		f = open("a.txt", "r")
+
+except OSError as e:
+	arg = input("Can't find a.txt, enter file name: ")
+	try:
+		
+		f = open(arg, "r")
+		b = 1
+	except OSError as e:
+		print("\nCould not find file\nExiting...")
+		a = 1
+
+if a == 0:
+	os.makedirs(os.path.dirname(dir), exist_ok=True)
+	print("\nDownloading "+str(len(f.readlines()))+" image(s)")
+	f.seek(0)
+	for line in f:
+		if line != "\n":
+			try:
+				# img = urllib.request.urlopen(line)
+				hdr = {'User-Agent': 'Mozilla/5.0'}
+				if "https://danbooru.donmai.us/posts?page=" in line:
+					continue
+				if "danbooru.donmai.us/posts?tags=" in line:
+					continue
+				if "https://yande.re/post?tags=" in line:
+					continue
+				if "danbooru.donmai.us/posts/" in line:
+					respose = requests.get(line)
+					data = respose.text
+					soup = BeautifulSoup(data, "html.parser")
+					for link in soup.find_all('a', href=re.compile('^/data/')):
+						line = "http://danbooru.donmai.us/" + link.get('href')
+
+				req = urllib.request.Request(line, headers=hdr)
+				img = urllib.request.urlopen(req)
+				temp = line.rpartition('/')[2]
+				temp = temp.strip('\n')
+				print("Downloading",temp)
+				out = open(dir+temp, "wb")
+				out.write(img.read())
+				out.close()
+			except urllib.error.HTTPError as err:
+				if err.code == 403:
+					print(err.code)
+				else:
+					raise
+	f.close()
+	if c == 1:
+		os.remove(sys.argv[1])
+	else:
+		if b == 1:
+			os.remove(arg)
+		else:
+			os.remove("a.txt")
+	print("\nDone!")
